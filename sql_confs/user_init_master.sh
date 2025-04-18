4d36c099c99e4a3d138eb902f08d6c6382f4e42c
pashkami <69209000+pashkami@users.noreply.github.com>
2025-04-18 20:53:28 +0300

    Update user_init_master.sh

--- sql_confs/user_init_master.sh
+++ sql_confs/user_init_master.sh
@@ -1,10 +1,4 @@
-#!/bin/bash
-
-echo "Создание пользователя репликации в мастер бд"
-
-mysql -uroot -p"$mysql_root_password" -e "
-  CREATE USER '{{ replica_user }}'@'%' identified by '{{ replica_password }}';
-  GRANT REPLICATION SLAVE ON *.* TO '{{ replica_user }}'@'%';
-  FLUSH PRIVILEGES;
-  FLUSH TABLES WITH READ LOCK;
-"
+CREATE USER '{{ replica_user }}'@'%' IDENTIFIED BY '{{ replica_password }}';
+GRANT REPLICATION SLAVE ON *.* TO '{{ replica_user }}'@'%';
+FLUSH PRIVILEGES;
+FLUSH TABLES WITH READ LOCK;
